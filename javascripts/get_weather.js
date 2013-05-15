5fc1c389ae2c0509949321aa31c4d4d03614e9d8
stsysr <sss846@gmail.com>
2013-05-15 11:53:56 +0900

    Create get_weather.js

---
+++ javascripts/get_weather.js
@@ -0,0 +1,18 @@
+$(function() {
+  navigator.geolocation.getCurrentPosition(OK, NG);
+  function OK(position) {
+    var url = ['http://openweathermap.org/data/2.1/find/city?',
+               'lat=' + position.coords.latitude,
+               '&lon=' + position.coords.longitude,
+               '&cnt=1&callback=?'].join('');
+    $.getJSON(url, function(data){
+      var info = ['緯度：' + position.coords.latitude,
+                  '経度：' + position.coords.longitude,
+                  '<img src="http://openweathermap.org/img/w/' + data.list[0].weather[0].icon + '.png">'].join('<br>');
+      $("span.result").html(info);
+    });
+  }
+  function NG(error) {
+    $("span.result").html('エラー！');
+  }
+});
