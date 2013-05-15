892f708f3c8d1221228f09dbfee558f5e940fd4f
stsysr <sss846@gmail.com>
2013-05-15 18:25:24 +0900

    Create test.js

---
+++ spec/test.js
@@ -0,0 +1,31 @@
+/*
+  node
+  require.js
+  mocha
+  expect
+  
+  $ npm install -g mocha
+  $ npm install expect.js
+*/
+
+var assert = require("assert")
+var expect = require('expect.js');
+
+describe('Array', function(){
+  describe('#indexOf()', function(){
+    it('should return -1 when the value is not present', function(){
+      assert.equal(-1, [1,2,3].indexOf(5));
+      assert.equal(-1, [1,2,3].indexOf(0));
+    })
+  })
+})
+
+// expect style
+// see https://github.com/LearnBoost/expect.js#api
+describe('Array', function(){
+  describe('#indexOf()', function(){
+    it('should return -1 when the value is not present', function(){
+      expect([1,2,3].indexOf(5)).to.eql(-1);
+    })
+  })
+})
