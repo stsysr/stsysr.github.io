6598a571245dabb92aed6537740fa0b3d9ec90de
stsysr <sss846@gmail.com>
2013-05-15 12:27:58 +0900

    Update get_weather.js

--- javascripts/get_weather.js
+++ javascripts/get_weather.js
@@ -1,14 +1,13 @@
 $(function() {
   navigator.geolocation.getCurrentPosition(OK, NG);
   function OK(position) {
-    var url = ['http://openweathermap.org/data/2.1/find/city?',
+    var url = ['http://api.openweathermap.org/data/2.5/weather?'
                'lat=' + position.coords.latitude,
-               '&lon=' + position.coords.longitude,
-               '&cnt=1&callback=?'].join('');
+               '&lon=' + position.coords.longitude].join('');
     $.getJSON(url, function(data){
       var info = ['緯度：' + position.coords.latitude,
                   '経度：' + position.coords.longitude,
-                  //position.sys.country,
+                  data.name,
                   data.list[0].weather[0].description,
                   '<img src="http://openweathermap.org/img/w/' + data.list[0].weather[0].icon + '.png">'].join('<br>');
       $("span.result").html(info);
