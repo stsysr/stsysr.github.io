334fbcbd4645d810b448144e68481ff0f2c61f26
stsysr <sss846@gmail.com>
2013-05-15 12:17:17 +0900

    Update get_weather.js

--- javascripts/get_weather.js
+++ javascripts/get_weather.js
@@ -8,7 +8,7 @@ $(function() {
     $.getJSON(url, function(data){
       var info = ['緯度：' + position.coords.latitude,
                   '経度：' + position.coords.longitude,
-                  position.sys.country,
+                  //position.sys.country,
                   data.list[0].weather[0].description,
                   '<img src="http://openweathermap.org/img/w/' + data.list[0].weather[0].icon + '.png">'].join('<br>');
       $("span.result").html(info);
