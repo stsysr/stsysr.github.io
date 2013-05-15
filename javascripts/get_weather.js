dedc89d246823e3912e0ae5c89edf168a75b86f6
stsysr <sss846@gmail.com>
2013-05-15 12:43:31 +0900

    Update get_weather.js

--- javascripts/get_weather.js
+++ javascripts/get_weather.js
@@ -8,10 +8,10 @@ $(function() {
     $.getJSON(url, function(data){
       var info = ['緯度：' + data.coord.lat,
                   '経度：' + data.coord.lon,
-                  data.sys.country,
-                  data.name,
-                  data.weather[0].description,
-                  data.main.humidity + '%',
+                  '国：' + data.sys.country,
+                  '都市：' + data.name,
+                  '湿度：' + data.main.humidity + '%',
+                  '天気：' + data.weather[0].description,
                   '<img src="http://openweathermap.org/img/w/' + data.weather[0].icon + '.png">'].join('<br>');
       $("span.result").html(info);
     });
