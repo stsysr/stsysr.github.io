cfd11f4d71ae3d84f571db5fabef225d9427d228
stsysr <sss846@gmail.com>
2013-05-17 14:16:30 +0900

    Update get_weather.js

--- javascripts/get_weather.js
+++ javascripts/get_weather.js
@@ -19,25 +19,25 @@ $(function() {
     var forecast = ['http://api.openweathermap.org/data/2.5/forecast?',
                     'lat=' + position.coords.latitude,
                     '&lon=' + position.coords.longitude].join('');
-    $.getJSON(current, function(data){
-      var info = ['latitude: ' + data.coord.lat,
-                  'longitude: ' + data.coord.lon,
-                  'country: ' + data.sys.country,
-                  'city: ' + data.name,
-                  'weather: ' + data.weather[0].main,
-                  'temperature: ' + KelvinToCelsius(data.main.temp) + '°C',
-                  'max temperature: ' + KelvinToCelsius(data.main.temp_max) + '°C',
-                  'min temperature: ' + KelvinToCelsius(data.main.temp_min) + '°C',
-                  'humidity: ' + data.main.humidity + '%',
-                  'pressure: ' + data.main.pressure + 'hpa',
-                  'sunrise: ' + data.sys.sunrise,
-                  'sunset: ' + data.sys.sunset,
-                  'wind: ' + data.wind.speed + 'm/s',
-                  'deg: ' + getWindDirection(data.wind.deg) + '/' + data.wind.deg + '°',
-                  'description: ' + data.weather[0].description,
-                  '<img src="http://openweathermap.org/img/w/' + data.weather[0].icon + '.png">'].join('<br>');
-      $("div.current").html(info);
-    });
+    // $.getJSON(current, function(data){
+    //   var info = ['latitude: ' + data.coord.lat,
+    //               'longitude: ' + data.coord.lon,
+    //               'country: ' + data.sys.country,
+    //               'city: ' + data.name,
+    //               'weather: ' + data.weather[0].main,
+    //               'temperature: ' + KelvinToCelsius(data.main.temp) + '°C',
+    //               'max temperature: ' + KelvinToCelsius(data.main.temp_max) + '°C',
+    //               'min temperature: ' + KelvinToCelsius(data.main.temp_min) + '°C',
+    //               'humidity: ' + data.main.humidity + '%',
+    //               'pressure: ' + data.main.pressure + 'hpa',
+    //               'sunrise: ' + data.sys.sunrise,
+    //               'sunset: ' + data.sys.sunset,
+    //               'wind: ' + data.wind.speed + 'm/s',
+    //               'deg: ' + getWindDirection(data.wind.deg) + '/' + data.wind.deg + '°',
+    //               'description: ' + data.weather[0].description,
+    //               '<img src="http://openweathermap.org/img/w/' + data.weather[0].icon + '.png">'].join('<br>');
+    //   $("div.current").html(info);
+    // });
     $.getJSON(forecast, function(data){
       var icon = '<img src="http://openweathermap.org/img/w/' + data.weather[0].icon + '.png">';
       $("div.forecast").html(icon);
