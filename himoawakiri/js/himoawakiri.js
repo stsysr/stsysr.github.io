6c94b205e7627b156b511644bdb8546593a9218f
stsysr <sss846@gmail.com>
2013-05-20 12:07:22 +0900

    Update himoawakiri.js

--- himoawakiri/js/himoawakiri.js
+++ himoawakiri/js/himoawakiri.js
@@ -1,6 +1,6 @@
 
 $(function() {
-  var url = 'http://himoawakiri.tumblr.com/api/read/json?num=100';
+  var url = 'http://himoawakiri.tumblr.com/api/read/json?num=100&cnt=1&callback=?';
   $.getJSON(url, function(data){
     console.log(data.tumblelog);
   });
