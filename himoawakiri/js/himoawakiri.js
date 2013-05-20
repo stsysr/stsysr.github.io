347da3c55e4cbb21a5fd997503d6e04c926dcc3d
stsysr <sss846@gmail.com>
2013-05-20 12:11:41 +0900

    Update himoawakiri.js

--- himoawakiri/js/himoawakiri.js
+++ himoawakiri/js/himoawakiri.js
@@ -1,6 +1,8 @@
 
 $(function() {
   var url_himo = 'http://himoawakiri.tumblr.com/api/read/json?num=100&cnt=1&callback=?';
+  var url_awa = 'http://awakirihimo.tumblr.com/api/read/json?num=100&cnt=1&callback=?';
+  var url_kiri = 'http://kirihimoawa.tumblr.com/api/read/json?num=100&cnt=1&callback=?';
   $.getJSON(url_himo, function(data_himo){
     console.log(data_himo.tumblelog);
   });
